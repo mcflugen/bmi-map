14e676164142b6c4f57a55ce5576204055124f43
mcflugen <mcflugen@gmail.com>
2024-11-07 16:02:36 -0700

    add simple cli test

---
+++ tests/cli_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+from bmi_map import main
+
+
+def test_help():
+    with pytest.raises(SystemExit):
+        main(["--help"])
+
+
+def test_version():
+    with pytest.raises(SystemExit):
+        main(["--version"])
