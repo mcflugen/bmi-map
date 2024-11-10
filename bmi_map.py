2e687eb94932573fa1d925eb7a139a161b91b998
mcflugen <mcflugen@gmail.com>
2024-11-10 09:26:11 -0700

    add a docstring and doctest for bmi_map

--- bmi_map.py
+++ bmi_map.py
@@ -62,6 +62,26 @@ def main(argv: Sequence[str] | None = None) -> int:
 def bmi_map(
     funcs: dict[str, dict[str, Sequence[dict[str, str]]]], to="sidl"
 ) -> list[str]:
+    """Map a BMI to a given language.
+
+    Parameters
+    ----------
+    funcs : dict
+        Interface definitions of BMI functions.
+    to : str, optional
+        Language to which to map the interface.
+
+    Examples
+    --------
+    >>> from bmi_map import bmi_map
+    >>> funcs = {
+    ...     "get_component_name": {
+    ...         "params": [dict(name="name", intent="in", type="string")]
+    ...     }
+    ... }
+    >>> bmi_map(funcs, to="c")
+    ['int get_component_name(void* self, const char* name);']
+    """
     LANGUAGE_MAPPER = {
         "c": CMapper,
         "c++": CxxMapper,
