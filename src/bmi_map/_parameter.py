e246c2e05626d0857db8af4a8e384be306e3435a
mcflugen <mcflugen@gmail.com>
2024-11-19 15:45:01 -0700

    move validator functions outside of Parameter class

--- src/bmi_map/_parameter.py
+++ src/bmi_map/_parameter.py
@@ -16,9 +16,9 @@ class Parameter:
     type: str
 
     def __post_init__(self):
-        self.name = self.validate_name(self.name)
-        self.intent = self.validate_intent(self.intent)
-        self.type = self.validate_type(self.type)
+        self.name = validate_name(self.name)
+        self.intent = validate_intent(self.intent)
+        self.type = validate_type(self.type)
 
     def asdict(self):
         return asdict(self)
@@ -28,59 +28,6 @@ class Parameter:
 
     def isscalar(self):
         return not self._type.startswith("array")
-
-    @staticmethod
-    def validate_name(name: str) -> str:
-        name = name.strip()
-        if not name.isidentifier():
-            raise ValueError(f"name is not valid ({name})")
-        return name
-
-    @staticmethod
-    def validate_intent(intent: str) -> str:
-        intent = intent.strip()
-        if intent not in VALID_INTENTS:
-            raise ValueError(
-                f"intent not understood ({intent} not one of"
-                f" {', '.join(repr(v) for v in sorted(VALID_INTENTS))})"
-            )
-        return intent
-
-    @staticmethod
-    def validate_scalar(dtype: str) -> str:
-        if dtype not in VALID_SCALAR_TYPES:
-            raise ValueError(
-                f"type not understood ({dtype!r} not one of"
-                f" {', '.join(repr(t) for t in sorted(VALID_SCALAR_TYPES))})"
-            )
-        return dtype
-
-    @staticmethod
-    def validate_array(dtype: str) -> str:
-        if not dtype.startswith("array"):
-            raise ValueError("not an array type ({dtype})")
-
-        dtype, dims = Parameter.split_array_type(dtype)
-        if dtype not in VALID_ARRAY_TYPES:
-            raise ValueError(
-                f"array type not understood ({dtype} not one of"
-                f" {', '.join(repr(t) for t in sorted(VALID_ARRAY_TYPES))})"
-            )
-        repeated_dims = [dim for dim, count in Counter(dims).items() if count > 1]
-        if repeated_dims:
-            raise ValueError(
-                f"repeated dimension{'s' if len(repeated_dims) > 1 else ''}"
-                f" ({', '.join(repeated_dims)})"
-            )
-
-        return f"array[{', '.join((dtype,) + dims)}]"
-
-    @staticmethod
-    def validate_type(dtype: str) -> str:
-        if dtype.startswith("array"):
-            return Parameter.validate_array(dtype)
-        else:
-            return Parameter.validate_scalar(dtype)
 
     @staticmethod
     def split_array_type(array_type: str) -> tuple[str, tuple[str, ...]]:
@@ -96,3 +43,56 @@ class Parameter:
             return dtype, dims
         else:
             raise ValueError(f"type not understood ({array_type})")
+
+
+def validate_name(name: str) -> str:
+    name = name.strip()
+    if not name.isidentifier():
+        raise ValueError(f"name is not valid ({name})")
+    return name
+
+
+def validate_intent(intent: str) -> str:
+    intent = intent.strip()
+    if intent not in VALID_INTENTS:
+        raise ValueError(
+            f"intent not understood ({intent} not one of"
+            f" {', '.join(repr(v) for v in sorted(VALID_INTENTS))})"
+        )
+    return intent
+
+
+def validate_scalar(dtype: str) -> str:
+    if dtype not in VALID_SCALAR_TYPES:
+        raise ValueError(
+            f"type not understood ({dtype!r} not one of"
+            f" {', '.join(repr(t) for t in sorted(VALID_SCALAR_TYPES))})"
+        )
+    return dtype
+
+
+def validate_array(dtype: str) -> str:
+    if not dtype.startswith("array"):
+        raise ValueError("not an array type ({dtype})")
+
+    dtype, dims = Parameter.split_array_type(dtype)
+    if dtype not in VALID_ARRAY_TYPES:
+        raise ValueError(
+            f"array type not understood ({dtype} not one of"
+            f" {', '.join(repr(t) for t in sorted(VALID_ARRAY_TYPES))})"
+        )
+    repeated_dims = [dim for dim, count in Counter(dims).items() if count > 1]
+    if repeated_dims:
+        raise ValueError(
+            f"repeated dimension{'s' if len(repeated_dims) > 1 else ''}"
+            f" ({', '.join(repeated_dims)})"
+        )
+
+    return f"array[{', '.join((dtype,) + dims)}]"
+
+
+def validate_type(dtype: str) -> str:
+    if dtype.startswith("array"):
+        return validate_array(dtype)
+    else:
+        return validate_scalar(dtype)
